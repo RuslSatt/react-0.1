1b8d996f845763ea0c8fbb707908131e767d0ec6
Ruslan Sattarov <r8sattarov@mail.ru>
2022-12-01 19:27:14 +0500

    feat: add new components

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,36 +1,6 @@
-import {useState} from "react";
 import {FaTrashAlt} from "react-icons/fa";
 
-const Content = () => {
-	const [items, setItems] = useState([
-		{
-			id: '1',
-			name: 'Bob',
-			checked: true,
-		},
-		{
-			id: '2',
-			name: 'Max',
-			checked: false,
-		},
-	]);
-
-	const handleCheck = (id) => {
-		const list = [...items];
-		const item = list.find((item) => item.id === id);
-		if (item) {
-			item.checked = !item.checked;
-			setItems(list);
-		}
-		localStorage.setItem('list', JSON.stringify(list))
-	}
-
-	const handleDelete = (id) => {
-		const list = items.filter((item) => item.id !== id);
-		setItems(list);
-		localStorage.setItem('list', JSON.stringify(list))
-	}
-
+const Content = ({items, handleCheck, handleDelete}) => {
 	return (
 		<main>
 			{items.length ? (
