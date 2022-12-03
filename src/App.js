c4f65c5535a58771d0a874aaec68655539f2a50f
Ruslan Sattarov <r8sattarov@mail.ru>
2022-12-03 18:17:15 +0500

    feat: add useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import './normalize.css';
 import './App.css';
-import Box from "./Components/Box";
-import {useState} from "react";
-import Input from "./Components/Input";
+import Header from "./Components/Header";
+import Content from "./Components/Content/Content";
+import Footer from "./Components/Footer";
+import {useEffect, useState} from "react";
+import AddItem from "./Components/Content/AddItem";
+import SearchItem from "./Components/Content/SearchItem";
 
 function App() {
-	const [value, setValue] = useState('');
-	const [isDarkText, setIsDarkText] = useState(true);
+	const local = JSON.parse(localStorage.getItem('shoppingList'))
+	const [items, setItems] = useState(local || []);
 
-	const handleSubmitColor = (e, item) => {
-		e.preventDefault();
-		setValue(item)
+	const [newItem, setNewItem] = useState('');
+	const [search, setSearch] = useState('')
+
+
+	useEffect(() => {
+		localStorage.setItem('shoppingList', JSON.stringify(items))
+	}, [items])
+
+	const handleCheck = (id) => {
+		const list = [...items];
+		const item = list.find((item) => item.id === id);
+		if (item) {
+			item.checked = !item.checked;
+			setItems(list)
+		}
 	}
+
+	const handleDelete = (id) => {
+		const list = items.filter((item) => item.id !== id);
+		setItems(list)
+	}
+
+	const addItem = (item) => {
+		const id = items.length ? items[items.length - 1].id + 1 : 1;
+		const myNewItem = {id, checked: false, item};
+		const list = [...items, myNewItem];
+		setItems(list)
+	}
+
+	const handleSumbit = (e) => {
+		e.preventDefault();
+		if (!newItem) return
+		addItem(newItem)
+		setNewItem('');
+	}
+
+	const sortItems = items.filter((item) => item.item.toLowerCase().includes(search.toLowerCase()));
 
 	return (
 		<div className="App">
-			<Box value={value} isDarkText={isDarkText}></Box>
-			<Input
-				handleSubmitColor={handleSubmitColor}
-				isDarkText={isDarkText}
-				setIsDarkText={setIsDarkText}
-			></Input>
+			<Header className="Header"></Header>
+			<AddItem
+				newItem={newItem}
+				setNewItem={setNewItem}
+				handleSumbit={handleSumbit}
+			></AddItem>
+			<SearchItem
+				search={search}
+				setSearch={setSearch}
+			></SearchItem>
+			<Content items={sortItems}
+					 handleCheck={handleCheck}
+					 handleDelete={handleDelete}>
+			</Content>
+			<Footer count={items?.length}></Footer>
 		</div>
 	);
 }
