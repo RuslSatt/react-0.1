707af83de46f5190b147e692f3c1220fb50510c3
Ruslan Sattarov <r8sattarov@mail.ru>
2022-12-12 21:06:03 +0500

    add main pages

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import Header from "./Components/Header";
 import ToolBar from "./Components/ToolBar";
 import {useState} from "react";
-import Content from "./Components/Content/Content";
 import Footer from "./Components/Footer";
 import styled from "styled-components";
+import Home from "./Components/Content/Home";
+import Post from "./Components/Content/Post";
+import About from "./Components/Content/About";
+import {Route, Routes} from "react-router-dom";
 
 // npx json-server -p 3100 -w data/db.json
 
@@ -15,9 +18,16 @@ function App() {
 		<div className="App">
 			<Header></Header>
 			<ToolBar value={value} setValue={setValue}></ToolBar>
+
 			<Main>
-				<Content></Content>
+				<Routes>
+					<Route path="/" element={<Home/>}/>
+					<Route path="/post" element={<Post/>}/>
+					<Route path="/about" element={<About/>}/>
+				</Routes>
 			</Main>
+
+
 			<Footer></Footer>
 		</div>
 	);
