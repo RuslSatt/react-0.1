ef4d27f9c9acdd8c107d4e305f3d1af78edf1f4c
Ruslan Sattarov <r8sattarov@mail.ru>
2023-01-16 20:38:12 +0500

    feat: add about page

--- src/Components/Content/About.js
+++ src/Components/Content/About.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Main} from "./Home";
+import { Main } from './Home';
+import styles from './Content.module.scss';
 
 const About = () => {
-	return (
-		<Main>
-
-		</Main>
-	);
+    return (
+        <Main className={styles.main}>
+            <h2 className={styles.postTitle}>About</h2>
+            <p className={styles.nameBlock}>This blog about posts</p>
+        </Main>
+    );
 };
 
 export default About;
