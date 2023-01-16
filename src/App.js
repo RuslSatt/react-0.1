54bf4c46dc63a44212c7d470d699dd082bd32314
Ruslan Sattarov <r8sattarov@mail.ru>
2023-01-16 21:03:13 +0500

    feat: add axios
    This comment add axios for fetch data

--- src/App.js
+++ src/App.js
@@ -9,36 +9,12 @@ import Post from './Components/Content/Post';
 import About from './Components/Content/About';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import PostPage from './Components/Content/PostPage';
+import api from './api/posts';
 
 // npx json-server -p 3100 -w data/db.json
 
 function App() {
-    const [posts, setPosts] = useState([
-        {
-            id: 1,
-            title: 'My First Post',
-            datetime: 'July 01, 2021 11:17:36 AM',
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-        },
-        {
-            id: 2,
-            title: 'My 2nd Post',
-            datetime: 'July 01, 2021 11:17:36 AM',
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-        },
-        {
-            id: 3,
-            title: 'My 3rd Post',
-            datetime: 'July 01, 2021 11:17:36 AM',
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-        },
-        {
-            id: 4,
-            title: 'My Fourth Post',
-            datetime: 'July 01, 2021 11:17:36 AM',
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-        },
-    ]);
+    const [posts, setPosts] = useState([]);
     const [search, setSearch] = useState('');
     const [searchResult, setSearchResult] = useState([]);
     const navigate = useNavigate();
@@ -64,7 +40,17 @@ function App() {
         setPosts(list);
     };
 
-    // const filteredPosts = posts.filter(item => item.title.toLowerCase().includes(search.toLowerCase()))
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await api.get('/posts');
+                setPosts(response.data);
+            } catch (err) {
+                console.log(err.response.data);
+            }
+        };
+        fetchPosts();
+    }, []);
 
     useEffect(() => {
         const filteredResults = posts.filter(model => {
